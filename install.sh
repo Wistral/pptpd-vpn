3af6b5174de3821f25d98a4d8e6c50a20e538b61
XCRobo3D <40349434+XCRobo3D@users.noreply.github.com>
2019-01-10 12:02:48 +0800

    auto-detect network card

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install ssh pptpd iptables -y
+sudo cp pptpd.conf /etc/pptpd.conf
+sudo cp pptpd-options /etc/ppp/pptpd-options
+#在chap-secrets修改账户名和密码
+sudo cp chap-secrets /etc/ppp/chap-secrets
+sudo cp sysctl.conf /etc/sysctl.conf
+#sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+sudo sysctl -p
+sudo ./rc.pptpd 
+
+
