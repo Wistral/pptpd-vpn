cd9d53210cdfde52983679487bcf77c19eef1fda
Wistral <whistral@gmail.com>
2019-06-23 16:18:11 +0800

    auto run

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install ssh pptpd iptables -y
-sudo cp pptpd.conf /etc/pptpd.conf
-sudo cp pptpd-options /etc/ppp/pptpd-options
-#在chap-secrets修改账户名和密码
-sudo cp chap-secrets /etc/ppp/chap-secrets
-sudo cp sysctl.conf /etc/sysctl.conf
-#sudo sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-sudo sysctl -p
-sudo ./pptpd.rc
-
-
+apt-get update
+apt-get install pptpd iptables -y
+cp pptpd.conf /etc/pptpd.conf
+cp pptpd-options /etc/ppp/pptpd-options
+cp chap-secrets /etc/ppp/chap-secrets
+cp sysctl.conf /etc/sysctl.conf
+#sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+sysctl -p
+ln -s $(pwd)/rc.pptpd /etc/init.d/hfut-vpn
+update-rc.d hfut-vpn defaults 9
+service pptpd restart
